3a4bc5fb0c71ee8359209227dffe14b2d3d6af0f
Mark Parson <mark@nanobox.io>
2017-11-03 16:07:46 -0600

    Debugging complete

--- src/vue/base.vue
+++ src/vue/base.vue
@@ -1,57 +1,57 @@
 <script type="text/babel">
-// import choosePlatform      from './choose-platform'
-// import chooseCollaboration from './choose-collaboration'
-// import chooseSupport       from './choose-support'
-// import finalize            from './finalize'
-// import account             from './account'
-// import {x, flux, errors}   from 'lexi'
-// import Sequence            from 'sequence'
-// import Brain               from './brain'
+import choosePlatform      from './choose-platform'
+import chooseCollaboration from './choose-collaboration'
+import chooseSupport       from './choose-support'
+import finalize            from './finalize'
+import account             from './account'
+import {x, flux, errors}   from 'lexi'
+import Sequence            from 'sequence'
+import Brain               from './brain'
 
 export default {
   name  : 'guru',
-  // props : ['model', 'callbacks'],
-  // components : {x, choosePlatform, chooseCollaboration, chooseSupport, finalize, account, flux, errors},
-  // data() {
-  //   let brain = new Brain(this.model, this)
-  //   let obj =  {
-  //     firstItem   : brain.sequence.firstItem,
-  //     currentPage : brain.sequence.currentItem,
-  //     brain       : brain,
-  //     error       : '',
-  //   }
-  //   if( !this.model.user.isLoggedIn )
-  //     obj.currentPage = 'account'
-  //   return obj
-  // },
-  // methods    : {
-  //   nextSlide() {
-  //     this.brain.sequence.next()
-  //     this.currentPage = this.brain.sequence.currentItem
-  //   },
-  //   prevSlide() {
-  //     this.brain.sequence.prev()
-  //     this.currentPage = this.brain.sequence.currentItem
-  //   },
-  //   submitPlans(paymentInfo, cb) {
-  //     this.callbacks.setPlans(this.model.user.currentPlans, paymentInfo, (data)=>{
-  //       cb()
-  //       this.error = data.error
-  //     })
-  //   },
-  //   onError(error){
-  //     this.error = error
-  //   }
-  // },
-  // computed:{
-  //   loggedIn(){return this.model.user.isLoggedIn},
-  // },
-  // watch:{
-  //   loggedIn(isLoggedIn){
-  //     if(isLoggedIn)
-  //       this.brain.handleLoggedIn()
-  //   }
-  // }
+  props : ['model', 'callbacks'],
+  components : {x, choosePlatform, chooseCollaboration, chooseSupport, finalize, account, flux, errors},
+  data() {
+    let brain = new Brain(this.model, this)
+    let obj =  {
+      firstItem   : brain.sequence.firstItem,
+      currentPage : brain.sequence.currentItem,
+      brain       : brain,
+      error       : '',
+    }
+    if( !this.model.user.isLoggedIn )
+      obj.currentPage = 'account'
+    return obj
+  },
+  methods    : {
+    nextSlide() {
+      this.brain.sequence.next()
+      this.currentPage = this.brain.sequence.currentItem
+    },
+    prevSlide() {
+      this.brain.sequence.prev()
+      this.currentPage = this.brain.sequence.currentItem
+    },
+    submitPlans(paymentInfo, cb) {
+      this.callbacks.setPlans(this.model.user.currentPlans, paymentInfo, (data)=>{
+        cb()
+        this.error = data.error
+      })
+    },
+    onError(error){
+      this.error = error
+    }
+  },
+  computed:{
+    loggedIn(){return this.model.user.isLoggedIn},
+  },
+  watch:{
+    loggedIn(isLoggedIn){
+      if(isLoggedIn)
+        this.brain.handleLoggedIn()
+    }
+  }
 }
 </script>
 
@@ -61,13 +61,13 @@ export default {
 
 <template lang="pug">
   .guru(v-bind:class="{account:currentPage == 'account'}")
-    //- x.close(@click="callbacks.close")
-    //- errors(:errors="error")
-    //- account(v-if="currentPage == 'account'" model="model" @register="callbacks.register" @login="callbacks.login" @forgot="callbacks.resetPassword" key="account" @error="onError")
-    //- choose-platform(:model="model" v-if="currentPage == 'platform'" @next="nextSlide" @prev="prevSlide" key="platform" v-bind:class="{first:firstItem == 'platform'}" )
-    //- choose-collaboration(:model="model" v-if="currentPage == 'collaboration'" @next="nextSlide" @prev="prevSlide" key="collaboration" v-bind:class="{first:firstItem == 'collaboration'}" )
-    //- choose-support(:model="model" v-if="currentPage == 'support'" @next="nextSlide" @prev="prevSlide" key="support" v-bind:class="{first:firstItem == 'support'}" )
-    //- finalize(:model="model" v-if="currentPage == 'finalize'" @change="currentPage = arguments[0]" @prev="prevSlide" key="finalize" v-bind:class="{first:firstItem == 'finalize'}" @submit="submitPlans" @error="onError")
+    x.close(@click="callbacks.close")
+    errors(:errors="error")
+    account(v-if="currentPage == 'account'" model="model" @register="callbacks.register" @login="callbacks.login" @forgot="callbacks.resetPassword" key="account" @error="onError")
+    choose-platform(:model="model" v-if="currentPage == 'platform'" @next="nextSlide" @prev="prevSlide" key="platform" v-bind:class="{first:firstItem == 'platform'}" )
+    choose-collaboration(:model="model" v-if="currentPage == 'collaboration'" @next="nextSlide" @prev="prevSlide" key="collaboration" v-bind:class="{first:firstItem == 'collaboration'}" )
+    choose-support(:model="model" v-if="currentPage == 'support'" @next="nextSlide" @prev="prevSlide" key="support" v-bind:class="{first:firstItem == 'support'}" )
+    finalize(:model="model" v-if="currentPage == 'finalize'" @change="currentPage = arguments[0]" @prev="prevSlide" key="finalize" v-bind:class="{first:firstItem == 'finalize'}" @submit="submitPlans" @error="onError")
 </template>
 
 <!--
